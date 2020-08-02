2496ac171cbb14a1e7672df6f5b0d3fb9d99d012
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-02 21:56:17 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ hash = {}
   cart.each do |item_hash|
     item_hash.each do |name, price_hash|
       if hash[name].nil?
-        hash[name] = price_hash.merge({:count => 1})
+        hash[name] = price_hash.join({:count => 1})
       else
         hash[name][:count] += 1
       end
