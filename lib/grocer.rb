e3a4a5bd82ffcbb22c71bb9f82e2c9f3bbab9c7a
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-02 21:59:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,6 +15,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-while i < cart do 
-  
+i = 0
+consolidate = {}
+while i < cart.count do 
 end
